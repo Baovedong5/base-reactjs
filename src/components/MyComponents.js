5b27939cf0aab73e90bd671468ff9f12250b39e3
Phuong <lephuong10142002@gmail.com>
2022-08-26 19:25:41 +0700

    video 16

---
+++ src/components/MyComponents.js
@@ -0,0 +1,18 @@
+//class component
+//function component
+
+import React from "react";
+
+class MyComponet extends React.Component {
+  //JSX
+  render() {
+    return (
+      <div>
+        my first component
+        {Math.random()}
+      </div>
+    );
+  }
+}
+
+export default MyComponet;
