1fc8584df594eb4206a671cd29855770bf83a72e
Phuong <lephuong10142002@gmail.com>
2022-08-27 19:37:18 +0700

    video 27

--- src/components/DisplayInfor.js
+++ src/components/DisplayInfor.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "./Displayinfor.scss";
 
 class DisplayInfor extends React.Component {
   state = {
@@ -16,7 +17,7 @@ class DisplayInfor extends React.Component {
     const { listUsers } = this.props;
     console.log(listUsers);
     return (
-      <div>
+      <div className="display-info-container">
         <div>
           <span
             onClick={() => {
