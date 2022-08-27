f0074b71256ad4aaf11f30fabb646b3e221884b9
Phuong <lephuong10142002@gmail.com>
2022-08-27 08:50:07 +0700

    video 23

--- src/components/MyComponents.js
+++ src/components/MyComponents.js
@@ -2,14 +2,21 @@
 //function component
 
 import React from "react";
+import DisplayInfor from "./DisplayInfor";
 import UserInfor from "./Userinfo";
 
 class MyComponet extends React.Component {
   //JSX
   render() {
+    const myInfor = ["ab", "c", "d"];
     return (
       <div>
         <UserInfor />
+        <br />
+        <br />
+        <DisplayInfor name="Hoi Dan IT" age="30" />
+        <hr />
+        <DisplayInfor name="Phuong" age={20} myInfor={myInfor} />
       </div>
     );
   }
