0f1b1867af188b1109de5fb9c440ea1034c065b2
Phuong <lephuong10142002@gmail.com>
2022-08-29 13:49:14 +0700

    video 34

--- src/components/DisplayInfor.js
+++ src/components/DisplayInfor.js
@@ -2,87 +2,71 @@ import React from "react";
 import "./Displayinfor.scss";
 import logo1 from "./../logo.svg";
 
-class DisplayInfor extends React.Component {
-  constructor(props) {
-    console.log(">>> call constructor: 1");
-    super(props);
-    this.state = {
-      isShowLitsUser: true,
-    };
-  }
+// class DisplayInfor extends React.Component {
+//   render() {
+//     console.log(">>> call me render");
+//     //props => viet tat propperties
+//     const { listUsers } = this.props;
+//     // console.log(listUsers);
+//     return (
+//       <div className="display-info-container">
+//         {true && (
+//           <>
+//             {listUsers.map((user) => {
+//               return (
+//                 <div key={user.id} className={+user.age > 18 ? "green" : "red"}>
+//                   <div>My name is {user.name}</div>
+//                   <div>My age is {user.age}</div>
+//                   <div>
+//                     <button
+//                       onClick={() => this.props.handleDeleteUser(user.id)}
+//                     >
+//                       Delete
+//                     </button>
+//                   </div>
+//                   <hr />
+//                 </div>
+//               );
+//             })}
+//           </>
+//         )}
+//         {/* <div>My name is {name}</div>
+//         <div>My age is {age}</div>
+//         <hr />
+//         <div>My name is {name}</div>
+//         <div>My age is {age}</div>
+//         <hr />
+//         <div>My name is {name}</div>
+//         <div>My age is {age}</div> */}
+//       </div>
+//     );
+//   }
+// }
 
-  componentDidMount() {
-    console.log(">>> call me component  did mount");
-    setTimeout(() => {
-      document.title = "Phuong";
-    }, 3000);
-  }
-
-  componentDidUpdate(prevProps, prevState, snapshot) {
-    console.log(">>> call me component  did update", this.props, prevProps);
-    if (this.props.listUsers !== prevProps.listUsers) {
-      if (this.props.listUsers.length == 5) {
-        alert("me");
-      }
-    }
-  }
-
-  handleShowHide = () => {
-    this.setState({
-      isShowLitsUser: !this.state.isShowLitsUser,
-    });
-  };
-
-  render() {
-    console.log(">>> call me render");
-    //props => viet tat propperties
-    const { listUsers } = this.props;
-    // console.log(listUsers);
-    return (
-      <div className="display-info-container">
-        {/* <img src={logo1} /> */}
-        <div>
-          <span
-            onClick={() => {
-              this.handleShowHide();
-            }}
-          >
-            {this.state.isShowLitsUser === true
-              ? "Hide list user"
-              : "Show list user"}
-          </span>
-        </div>
-        {this.state.isShowLitsUser && (
-          <>
-            {listUsers.map((user) => {
-              return (
-                <div key={user.id} className={+user.age > 18 ? "green" : "red"}>
-                  <div>My name is {user.name}</div>
-                  <div>My age is {user.age}</div>
-                  <div>
-                    <button
-                      onClick={() => this.props.handleDeleteUser(user.id)}
-                    >
-                      Delete
-                    </button>
-                  </div>
-                  <hr />
+const DisplayInfor = (props) => {
+  const { listUsers } = props;
+  return (
+    <div className="display-info-container">
+      {true && (
+        <>
+          {listUsers.map((user) => {
+            return (
+              <div key={user.id} className={+user.age > 18 ? "green" : "red"}>
+                <div>My name is {user.name}</div>
+                <div>My age is {user.age}</div>
+                <div>
+                  <button onClick={() => props.handleDeleteUser(user.id)}>
+                    Delete
+                  </button>
                 </div>
-              );
-            })}
-          </>
-        )}
-        {/* <div>My name is {name}</div>
-        <div>My age is {age}</div>
-        <hr />
-        <div>My name is {name}</div>
-        <div>My age is {age}</div>
-        <hr />
-        <div>My name is {name}</div>
-        <div>My age is {age}</div> */}
-      </div>
-    );
-  }
-}
+                <hr />
+              </div>
+            );
+          })}
+        </>
+      )}
+    </div>
+  );
+};
 
 export default DisplayInfor;
