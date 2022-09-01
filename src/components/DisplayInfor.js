547281946cd0332e8f4fe0535c07a80496aa65fa
Phuong <lephuong10142002@gmail.com>
2022-09-01 16:30:17 +0700

    video 46

--- src/components/DisplayInfor.js
+++ src/components/DisplayInfor.js
@@ -1,47 +1,5 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./Displayinfor.scss";
-import logo1 from "./../logo.svg";
-
-// class DisplayInfor extends React.Component {
-//   render() {
-//     console.log(">>> call me render");
-//     //props => viet tat propperties
-//     const { listUsers } = this.props;
-//     // console.log(listUsers);
-//     return (
-//       <div className="display-info-container">
-//         {true && (
-//           <>
-//             {listUsers.map((user) => {
-//               return (
-//                 <div key={user.id} className={+user.age > 18 ? "green" : "red"}>
-//                   <div>My name is {user.name}</div>
-//                   <div>My age is {user.age}</div>
-//                   <div>
-//                     <button
-//                       onClick={() => this.props.handleDeleteUser(user.id)}
-//                     >
-//                       Delete
-//                     </button>
-//                   </div>
-//                   <hr />
-//                 </div>
-//               );
-//             })}
-//           </>
-//         )}
-//         {/* <div>My name is {name}</div>
-//         <div>My age is {age}</div>
-//         <hr />
-//         <div>My name is {name}</div>
-//         <div>My age is {age}</div>
-//         <hr />
-//         <div>My name is {name}</div>
-//         <div>My age is {age}</div> */}
-//       </div>
-//     );
-//   }
-// }
 
 const DisplayInfor = (props) => {
   const { listUsers } = props;
@@ -51,6 +9,15 @@ const DisplayInfor = (props) => {
   const handleShowHideListUser = () => {
     setShowHideListUser(!isShowHideListUser);
   };
+
+  console.log(">>> call me render");
+
+  useEffect(() => {
+    if (listUsers.length === 0) {
+      alert("You deleted all the user");
+    }
+    console.log(">>> call me useEffect");
+  }, [listUsers]);
 
   return (
     <div className="display-info-container">
