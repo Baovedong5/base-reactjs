4b8ea1b0e710ac0211ae09378d0eeb0cdcd8fe87
Phuong <lephuong10142002@gmail.com>
2022-09-20 15:53:34 +0700

    video 83

--- src/components/User/DetailQuiz.js
+++ src/components/User/DetailQuiz.js
@@ -1,11 +1,13 @@
 import { useEffect } from "react";
-import { useParams } from "react-router-dom";
+import { useParams, useLocation } from "react-router-dom";
 import { getDataQuiz } from "../../services/apiService";
 import _ from "lodash";
+import "./DetailQuiz.scss";
 
 const DetailQuiz = (props) => {
   const params = useParams();
   const quizId = params.id;
+  const location = useLocation();
 
   useEffect(() => {
     fetchQuestions();
@@ -13,13 +15,10 @@ const DetailQuiz = (props) => {
 
   const fetchQuestions = async () => {
     let res = await getDataQuiz(quizId);
-    console.log(">>> check question: ", res);
     if (res && res.EC === 0) {
       let raw = res.DT;
       let data = _.chain(raw)
-        // Group the elements of Array based on `color` property
         .groupBy("id")
-        // `key` is group's name (color), `value` is the array of objects
         .map((value, key) => {
           let answers = [];
           let questionDescription,
@@ -38,7 +37,32 @@ const DetailQuiz = (props) => {
     }
   };
 
-  return <div className="detail-quiz-container">DetailQuiz</div>;
+  return (
+    <div className="detail-quiz-container">
+      <div className="left-content">
+        <div className="title">
+          Quiz {quizId} : {location?.state?.quizTitle}
+        </div>
+        <hr />
+        <div className="q-body">
+          <img />
+        </div>
+        <div className="q-content">
+          <div className="question">Question 1: how are you doing?</div>
+          <div className="answer">
+            <div className="a-child">A. fhjfhjf</div>
+            <div className="a-child">B. fhjfhjf</div>
+            <div className="a-child">C. fhjfhjf</div>
+          </div>
+        </div>
+        <div className="footer">
+          <button className="btn btn-secondary">Prev</button>
+          <button className="btn btn-primary">Next</button>
+        </div>
+      </div>
+      <div className="right-content">count down</div>
+    </div>
+  );
 };
 
 export default DetailQuiz;
