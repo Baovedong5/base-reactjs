552c15f3b06d45610a81b093d7705569d5cb1b01
Phuong <lephuong10142002@gmail.com>
2022-10-12 14:35:21 +0700

    video 100

--- src/components/Admin/Content/Quiz/ManageQuiz copy.js
+++ src/components/Admin/Content/Quiz/ManageQuiz copy.js
@@ -5,8 +5,6 @@ import { postCreateNewQuiz } from "../../../../services/apiService";
 import { toast } from "react-toastify";
 import TableQuiz from "./TableQuiz";
 import Accordion from "react-bootstrap/Accordion";
-import QuizQA from "./QuizQA";
-import AssignQuiz from "./AssignQuiz";
 
 const options = [
   { value: "EASY", label: "EASY" },
@@ -105,15 +103,11 @@ const ManageQuiz = (props) => {
         </Accordion.Item>
         <Accordion.Item eventKey="1">
           <Accordion.Header>Update Q/A Quizzes</Accordion.Header>
-          <Accordion.Body>
-            <QuizQA />
-          </Accordion.Body>
+          <Accordion.Body></Accordion.Body>
         </Accordion.Item>
         <Accordion.Item eventKey="2">
           <Accordion.Header>Assign to Users</Accordion.Header>
-          <Accordion.Body>
-            <AssignQuiz />
-          </Accordion.Body>
+          <Accordion.Body></Accordion.Body>
         </Accordion.Item>
       </Accordion>
     </div>
