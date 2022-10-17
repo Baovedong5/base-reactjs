9461f6a0dffc30d689e6feadbc3c88dcf65b9153
Phuong <lephuong10142002@gmail.com>
2022-10-17 17:10:15 +0700

    video 105

--- src/components/User/Content/RightContent.js
+++ src/components/User/Content/RightContent.js
@@ -1,13 +1,46 @@
 import CountDown from "./CountDown";
+import { useRef } from "react";
 
 const RightContent = (props) => {
+  const refDiv = useRef([]);
   const { dataQuiz } = props;
 
   const onTimeUp = () => {
     props.handleFinishQuiz();
   };
 
-  console.log("DataQuiz", dataQuiz);
+  // console.log("DataQuiz", dataQuiz);
+
+  const getClassQuestion = (index, question) => {
+    //check answered
+    if (question && question.answers.length > 0) {
+      let isAnswered = question.answers.find((a) => a.isSelected === true);
+      if (isAnswered) {
+        return "question selected";
+      }
+    }
+    return "question";
+  };
+
+  const handleClickQuestion = (question, index) => {
+    props.setIndex(index);
+    if (refDiv.current) {
+      console.log(refDiv.current);
+      refDiv.current.forEach((item) => {
+        if (item && item.className === "question clicked") {
+          item.className = "question";
+        }
+      });
+    }
+    if (question && question.answers.length > 0) {
+      let isAnswered = question.answers.find((a) => a.isSelected === true);
+      if (isAnswered) {
+        return;
+      }
+    }
+    refDiv.current[index].className = "question clicked";
+  };
+
   return (
     <>
       <div className="main-timer">
@@ -18,7 +51,12 @@ const RightContent = (props) => {
           dataQuiz.length > 0 &&
           dataQuiz.map((item, index) => {
             return (
-              <div key={`question-abc-${index}`} className="question">
+              <div
+                key={`question-abc-${index}`}
+                className={getClassQuestion(index, item)}
+                onClick={() => handleClickQuestion(item, index)}
+                ref={(element) => (refDiv.current[index] = element)}
+              >
                 {index + 1}
               </div>
             );
